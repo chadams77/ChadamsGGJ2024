b75780076663281d1bd12e20a643cfbef1865532
Chris Adams <chris.ah.adams@gmail.com>
2024-01-26 20:20:20 -0400

    initial tileset, start to terrain class

--- main.cpp
+++ main.cpp
@@ -1,29 +1,116 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <map>
+#include <set>
+
 #include <SFML/Window.hpp>
 #include <SFML/Graphics.hpp>
 
 using namespace sf;
+using std::cerr;
+using std::cout;
+using std::endl;
+using std::vector;
+using std::map;
+using std::set;
 
 RenderWindow * window = NULL;
+
+class Terrain {
+public:
+    Texture tilesetTex;
+    Sprite tileset;
+    Image tilesetImg;
+    Texture renderedTex;
+    Sprite rendered;
+    uint32_t * bfr = NULL;
+    int width = 320, height = 240;
+    int tileSize = 16, tsSize = 128 / 16;
+
+    Terrain() {
+        if (!tilesetTex.loadFromFile("sprites/tiles.png")) {
+            cerr << "Tileset not found" << endl;
+            exit(0);
+        }
+
+        tilesetImg = tilesetTex.copyToImage();
+
+        tilesetTex.setSmooth(false);
+        tileset.setTexture(tilesetTex);
+        renderedTex.create(width, height);
+
+        bfr = new uint32_t[width * height];
+
+        resetLevel();
+
+        renderedTex.setSmooth(false);
+        rendered.setTexture(renderedTex);
+    }
+
+    void resetLevel() {
+        memset(bfr, 0, sizeof(uint32_t)*width*height);
+
+        drawTile(0, 0, 1, 1);
+        drawTile(1, 0, 2, 1);
+        drawTile(2, 0, 3, 1);
+        renderedTex.update((Uint8*)bfr);
+    }
+
+    void drawTile(int idX, int idY, int sx, int sy) {
+        int rx = idX * tileSize;
+        int ry = idY * tileSize;
+        uint32_t * ptr = (uint32_t*)tilesetImg.getPixelsPtr();
+        sx *= tileSize; sy *= tileSize;
+        int twpx = tileSize * tsSize;
+        for (int x=0; x<tileSize; x++) {
+            for (int y=0; y<tileSize; y++) {
+                int sx2 = x + sx, sy2 = y + sy;
+                if (sx2 >= 0 && sy2 >= 0 && sx2 < width && sy2 < height) {;
+                    bfr[sx2 + sy2 * width] = ptr[rx + x + (ry + y) * twpx];
+                }
+            }
+        }
+    }
+
+    ~Terrain() {
+        if (bfr != NULL) {
+            delete bfr;
+            bfr = NULL;
+        }
+    }
+
+    void render (double dt) {
+        window->draw(rendered);
+    }
+};
+
+Terrain * terrain = NULL;
 
 int main() {
 
     window = new RenderWindow(VideoMode(800, 600), "Test");
 
+    terrain = new Terrain();
+
     window->setFramerateLimit(60);
     
     while (window->isOpen()) {
-        sf::Event event;
+        Event event;
         while (window->pollEvent(event)) {
-            if (event.type == sf::Event::Closed) {
+            if (event.type == Event::Closed) {
                 window->close();
             }
         }
 
-        window->clear(Color::Red);
+        window->clear(Color::Black);
+
+        terrain->render(1. / 60.);
 
         window->display();
     }
 
+    delete terrain;
     delete window;
 
     return 0;
