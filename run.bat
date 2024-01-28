0f2f52a579479eff5e53b7a9042bbe298b61c537
Chris Adams <chris.ah.adams@gmail.com>
2024-01-28 12:13:40 -0400

    music

--- run.bat
+++ run.bat
@@ -1,5 +1,5 @@
-@del /Q build\chadams-ggj-2024.exe
-@CL /Os /Iinclude main.cpp /link opengl32.lib lib\sfml-window.lib lib\sfml-system.lib lib\sfml-network.lib lib\sfml-graphics.lib lib\sfml-audio.lib lib\openal32.lib lib\flac.lib lib\freetype.lib lib\ogg.lib lib\vorbis.lib lib\vorbisfile.lib lib\vorbisenc.lib /out:build/chadams-ggj-2024.exe
+@del /Q build\Notlsb.exe
+@CL /Os /Iinclude main.cpp /link opengl32.lib lib\sfml-window.lib lib\sfml-system.lib lib\sfml-network.lib lib\sfml-graphics.lib lib\sfml-audio.lib lib\openal32.lib lib\flac.lib lib\freetype.lib lib\ogg.lib lib\vorbis.lib lib\vorbisfile.lib lib\vorbisenc.lib /out:build/Notlsb.exe
 @del main.obj
 @del /Q build\shaders
 @del /Q build\sprites
@@ -8,5 +8,5 @@
 @xcopy sprites build\sprites /i /E
 @xcopy sfx build\sfx /i /E
 @cd build/
-@chadams-ggj-2024.exe
+@Notlsb.exe
 @cd ..
